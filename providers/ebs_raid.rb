606419900854a1404ed7a5e6295301c05f70b2b7
Yasha Portnoy <yportnoy@optoro.com>
2015-01-12 15:18:36 -0500

    Should fix a few problems

--- providers/ebs_raid.rb
+++ providers/ebs_raid.rb
@@ -343,7 +343,7 @@ def create_raid_disks(mount_point, mount_point_owner, mount_point_group, mount_p
   # For each volume add information to the mount metadata
   (1..num_disks).each do |i|
 
-    disk_dev_path = "/dev/#{find_free_volume_device_prefix}"
+    disk_dev_path = find_free_volume_device_prefix}
     Chef::Log.debug("vol device prefix is #{disk_dev_path}")
 
     Chef::Log.info "Snapshot array is #{snapshots[i-1]}"
